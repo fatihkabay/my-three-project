e34281ba9a60ec4a53ce718c62760c48b0afb2e1
Fatih Kabay <mfthkabay@gmail.com>
2023-09-02 23:44:39 +0300

    created mesh materials

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-import { Three } from "three"
+import * as THREE from "three"
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.SphereGeometry( 3, 64, 64 )
+const material = new THREE.MeshStandardMaterial({
+     color: "00ff83",
+
+})
+
+const mesh = new THREE.Mesh( geometry, material )
+scene.add( mesh )
