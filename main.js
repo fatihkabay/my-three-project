5a60844db47d2edbdb29d84c1f07486fcaa5cb4c
Fatih Kabay <mfthkabay@gmail.com>
2023-09-03 23:23:24 +0300

    renderer some items

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ const material = new THREE.MeshStandardMaterial({
 
 const mesh = new THREE.Mesh( geometry, material )
 scene.add( mesh )
+
+const camera = new THREE.PerspectiveCamera( 45, 800, 600 )
+scene.add( camera )
+
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({canvas})
+
+renderer.setSize( 800, 600 )
+
+renderer.render( scene, camera )
