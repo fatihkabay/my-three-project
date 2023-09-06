8e43321138f52c05c4ae93ca5855bb46c7426c84
Fatih Kabay <mfthkabay@gmail.com>
2023-09-06 23:35:14 +0300

    geometry error

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ const scene = new THREE.Scene()
 
 const geometry = new THREE.SphereGeometry( 3, 64, 64 )
 const material = new THREE.MeshStandardMaterial({
-     color: "00ff83",
+     color: "#00ff83",
 
 })
 
 const mesh = new THREE.Mesh( geometry, material )
 scene.add( mesh )
 
-const camera = new THREE.PerspectiveCamera( 45, 800, 600 )
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set( 0, 10, 10 )
+scene.add( light )
+
+const camera = new THREE.PerspectiveCamera( 45, 800 / 600 )
+camera.position.z = 20
 scene.add( camera )
 
 const canvas = document.querySelector('.webgl')
