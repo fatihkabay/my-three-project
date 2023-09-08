18b911d39b9b4ca621f9257f4c0020f14aa63d30
Fatih Kabay <mfthkabay@gmail.com>
2023-09-08 23:58:07 +0300

    fixed error

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-import * as THREE from "three"
+import * as THREE from 'three';
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const geometry = new THREE.SphereGeometry( 3, 64, 64 )
-const material = new THREE.MeshStandardMaterial({
-     color: "#00ff83",
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-})
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-const mesh = new THREE.Mesh( geometry, material )
-scene.add( mesh )
+camera.position.z = 5;
 
-const light = new THREE.PointLight(0xffffff, 1, 100)
-light.position.set( 0, 10, 10 )
-scene.add( light )
+function animate() {
+	requestAnimationFrame( animate );
 
-const camera = new THREE.PerspectiveCamera( 45, 800 / 600 )
-camera.position.z = 20
-scene.add( camera )
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
-const canvas = document.querySelector('.webgl')
-const renderer = new THREE.WebGLRenderer({canvas})
+	renderer.render( scene, camera );
+}
 
-renderer.setSize( 800, 600 )
-
-renderer.render( scene, camera )
+animate();
