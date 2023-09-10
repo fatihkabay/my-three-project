277dd368ca500de07de26476c33ced4c00827d94
Fatih Kabay <mfthkabay@gmail.com>
2023-09-10 22:41:40 +0300

    created canvas elements

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
